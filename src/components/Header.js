a79627170bd7f8d9f10f8ac8b941f78f6deea469
Silviu Iosif <6332838@rts-365.dk>
2021-01-20 21:47:02 +0100

    updating styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Header.scss";
 
-function Header({children}){
+function Header({children, theColor, theBackground}){
     return(
-        <article className="header">
+        <article className="header" style={{backgroundColor:`${theBackground}`}}>
             <button className="header__button"><img src="./icons/arrowBack.svg" alt="arrow back icon"/></button>
-            <h4 className="header__title">{children}</h4>
+            <h4 className="header__title" style={{color:`${theColor}`}}>{children}</h4>
             <div className="header__search"><img src="./icons/search.svg" alt="search icon"/></div>
         </article>
     )
