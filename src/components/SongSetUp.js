885f0f7e2558d0fd001a55940de4834cf839948f
Silviu Iosif <6332838@rts-365.dk>
2021-01-26 13:48:21 +0100

    add player branch

--- src/components/SongSetUp.js
+++ src/components/SongSetUp.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./SongSetUp.scss";
+import { Link } from "@reach/router";
+
 
 function SongSetUp({image, styleContainer,styleImg, gridSize, imageDescription, title, author, amount}){
     return(
         <article className="songWrapper" style={gridSize}>
-            <article style={styleContainer} className="songWrapper__imgContainer">
+            <Link to="/player" style={styleContainer} className="songWrapper__imgContainer">
                 <img style={styleImg} className="songWrapper__img" src={image} alt={imageDescription}/>
-            </article>
+            </Link>
             <section className="songWrapper__content">
                 <h5 className="title">{title}</h5>
                 <p className="author">{author}</p>
